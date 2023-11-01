ab6ea8121bcd05baa0bd2ca39a3a90124d73b5d5
S07C10 <S07C10@S07C10>
2023-11-01 08:08:37 +0330

    Add project files.

---
+++ github1/Program.cs
@@ -0,0 +1,33 @@
+﻿class program
+{
+    
+    static void Main()
+    {
+
+        Console.WriteLine(  "please enter a number :");
+        int num;
+        num = int.Parse(Console.ReadLine());
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            default:
+                Console.WriteLine("error");
+                break;
+        }
+
+
+
+
+
+
+    }
+
+}
